d5b6ef5612880a505e7622cdee9b8a3e34f8ebbd
Norman Lew <normanlew@users.noreply.github.com>
2026-02-04 15:15:59 -0800

    Complete 06_REPEATSTRING
    Completed string replete exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num === 0 || str === '') { return ""};
+    if (num < 0) {return "ERROR"};
+    var returnStr = str
+    while (num > 1) {
+        returnStr = returnStr + str;
+        // console.log(num + ": " + str);
+        num--;
+    }
 
+    return returnStr;
 };
 
 // Do not edit below this line
