9b38f1d995a2e2e344ea0e5be721b7316f82ae65
Norman Lew <normanlew@users.noreply.github.com>
2026-02-05 16:38:33 -0800

    Complete 09_sumAll
    Created function to sum all numbers between 2 paramaters.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number" || a === NaN || b === NaN || a < 0 || b < 0) return "ERROR";
+    return (Math.abs(b - a) + 1) * (b + a) / 2;
 };
 
 // Do not edit below this line
