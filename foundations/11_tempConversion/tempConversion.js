3bc2a1f9c671690066700928ec9b6c389c6b51ee
Norman Lew <normanlew@users.noreply.github.com>
2026-02-06 13:13:01 -0800

    Complete 11_tempConversion
    Wrote functions to convert celcius to fahrenheit and vice versa.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+// console.log("test");
+
+const convertToCelsius = function(tempFahrenheit) {
+  return Math.round(((tempFahrenheit - 32) * 5/9) * 10) / 10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+  return Math.round((tempCelcius * 9/5 + 32) * 10) / 10;
 };
+
+// console.log("test");
+
+// console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// x °C ≘ (x × ⁠9/5⁠ + 32) °F 	x °F ≘ (x − 32) × ⁠5/9⁠ °C 
