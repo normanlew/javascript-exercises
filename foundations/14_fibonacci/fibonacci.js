5be2ec4e75f1a8ac0d414f4d503fc6e4e45b4eb0
Norman Lew <normanlew@users.noreply.github.com>
2026-02-19 13:20:29 -0800

    Complete 14_fibonacci
    Used arrays to compute fibonacci number.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let number = Number(num);
+    let arr = [1, 1];
 
+    if (number < 0) {
+        return "OOPS";
+    }
+
+    if (number === 0) {
+        return 0;
+    }
+    if (number === 1 || number === 2) {
+        return arr[number - 1];
+    }
+    else {
+        while (number > 2) {
+            let temp = arr[1];
+            arr[1] = arr[0] + arr[1];
+            arr[0] = temp;
+            number--;
+        }
+    }
+    return arr[1];
 };
+    
 
 // Do not edit below this line
 module.exports = fibonacci;
